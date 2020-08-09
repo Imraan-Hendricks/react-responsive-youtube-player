e0ac3a8d524146ac1c6f8849411e88a771a20226
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-10 00:23:30 +0200

    added video gallery title and update styles

--- src/components/video-gallery/video-gallery.jsx
+++ src/components/video-gallery/video-gallery.jsx
@@ -5,21 +5,24 @@ import YoutubePlayer from '../youtube-player/youtube-player';
 const VideoGallery = (props) => {
   return (
     <div className={cssModule.root}>
-      {props.videos.map((video) => (
-        <div key={video.titleKey} className={cssModule.video}>
-          <YoutubePlayer
-            titleKey={video.titleKey}
-            autoplay={false}
-            controls={true}
-            fullscreen={true}
-            loop={false}
-            start={undefined}
-            end={undefined}
-            width={'100%'}
-            youtubeId={video.youtubeId}
-          />
-        </div>
-      ))}
+      <h1>Youtube Video Gallery</h1>
+      <div className={cssModule.gallery}>
+        {props.videos.map((video) => (
+          <div key={video.titleKey} className={cssModule.video}>
+            <YoutubePlayer
+              titleKey={video.titleKey}
+              autoplay={false}
+              controls={true}
+              fullscreen={true}
+              loop={false}
+              start={undefined}
+              end={undefined}
+              width={'100%'}
+              youtubeId={video.youtubeId}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
